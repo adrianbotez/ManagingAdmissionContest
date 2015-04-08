a915a769bb192a9f82d9c4bda765180b39fc7d2d
adrianbotez <adibotez@yahoo.com>
2015-04-08 18:45:16 +0300

    initial commit

---
+++ ManagingAdmissionContest/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ManagingAdmissionContest
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+        }
+    }
+}

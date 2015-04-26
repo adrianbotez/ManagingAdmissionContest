ed399079d1003aa2235cf99da5796d3956d24ca1
Adela Babinciuc <babinciuc.adela@gmail.com>
2015-04-26 23:50:42 +0300

    Database System Module update

--- ManagingAdmissionContest/Applicant.cs
+++ ManagingAdmissionContest/Applicant.cs
@@ -2,54 +2,110 @@
 
 namespace ManagingAdmissionContest
 {
+    /// <summary>
+    /// Manages a single applicant.
+    /// </summary>
     public class Applicant
     {
-        public string Cnp { get; set; }
+        /// <summary>
+        /// The id of the applicant
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The surname of the applicant
+        /// </summary>
         public string Surname { get; set; }
+
+        /// <summary>
+        /// The name of the applicant
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// The test grade of the applicant
+        /// </summary>
         public double TestGrade { get; set; }
+
+        /// <summary>
+        /// The bacalaureat grade of the applicant
+        /// </summary>
         public double BacGrade { get; set; }
+
+        /// <summary>
+        /// The informatics grade of the applicant
+        /// </summary>
         public double InfoGrade { get; set; }
+
+        /// <summary>
+        /// The mathematics grade of the applicant
+        /// </summary>
         public double MathGrade { get; set; }
 
+        /// <summary>
+        /// The admission grade of the applicant
+        /// </summary>
+        public double AdmissionGrade { get; set; }
+
+        /// <summary>
+        /// Creates an applicant object with default values.
+        /// </summary>
         public Applicant()
         {
-            this.Cnp = null;
-            this.Surname = null;
-            this.Name = null;
-            this.TestGrade = 0.0;
-            this.BacGrade = 0.0;
-            this.InfoGrade = 0.0;
-            this.MathGrade = 0.0;
+            Id = null;
+            Surname = null;
+            Name = null;
+            TestGrade = 0.0;
+            BacGrade = 0.0;
+            InfoGrade = 0.0;
+            MathGrade = 0.0;
+            AdmissionGrade = 0.0;
         }
 
-        public Applicant(string cnp, string surname, string name, double testGrade, double bacGrade, double infoGrade, double mathGrade)
+        /// <summary>
+        /// Creates an applicant object with given values for each property.
+        /// </summary>
+        /// <param name="id">The id of the applicant</param>
+        /// <param name="surname">The surname of the applicant</param>
+        /// <param name="name">The name of the applicant</param>
+        /// <param name="testGrade">The written test grade of the applicant</param>
+        /// <param name="bacGrade">The bacalaureat grade of the applicant</param>
+        /// <param name="infoGrade">The bacalaureat:informatics grade of the applicant</param>
+        /// <param name="mathGrade">The bacalaureat:mathematics grade of the applicant</param>
+        /// <param name="admissionGrade">The admission contest grade of the applicant</param>
+        public Applicant(string id, string surname, string name, double testGrade, double bacGrade, double infoGrade, double mathGrade, double admissionGrade)
         {
-            this.Cnp = cnp;
-            this.Surname = surname;
-            this.Name = name;
-            this.TestGrade = testGrade;
-            this.BacGrade = bacGrade;
-            this.InfoGrade = infoGrade;
-            this.MathGrade = mathGrade;
+            Id = id;
+            Surname = surname;
+            Name = name;
+            TestGrade = testGrade;
+            BacGrade = bacGrade;
+            InfoGrade = infoGrade;
+            MathGrade = mathGrade;
+            AdmissionGrade = admissionGrade;
         }
 
+        /// <summary>
+        /// Creates an applicant object from a single string.
+        /// </summary>
+        /// <param name="applicatData">A string with the data of the applicant</param>
         public Applicant(String applicatData)
         {
             string[] fields = applicatData.Split(',');
 
-            this.Cnp = fields[0];
-            this.Surname = fields[1];
-            this.Name = fields[2];
-            this.TestGrade = Convert.ToDouble(fields[3]);
-            this.BacGrade = Convert.ToDouble(fields[4]);
-            this.InfoGrade = Convert.ToDouble(fields[5]);
-            this.MathGrade = Convert.ToDouble(fields[6]);
+            Id = fields[0];
+            Surname = fields[1];
+            Name = fields[2];
+            TestGrade = Convert.ToDouble(fields[3]);
+            BacGrade = Convert.ToDouble(fields[4]);
+            InfoGrade = Convert.ToDouble(fields[5]);
+            MathGrade = Convert.ToDouble(fields[6]);
+            AdmissionGrade = Convert.ToDouble(fields[7]);
         }
 
         public override string ToString()
         {
-            string s = Cnp + "," + Surname + "," + Name + "," + TestGrade + "," + BacGrade + "," + InfoGrade + "," + MathGrade;
+            string s = Id + "," + Surname + "," + Name + "," + TestGrade + "," + BacGrade + "," + InfoGrade + "," + MathGrade + "," + AdmissionGrade;
             return s;
         }
         public override bool Equals(Object obj)
@@ -58,12 +114,12 @@ namespace ManagingAdmissionContest
             if (applicant == null)
                 return false;
             else
-                return Cnp.Equals(applicant.Cnp);
+                return Id.Equals(applicant.Id);
         }
 
         public override int GetHashCode()
         {
-            return Int32.Parse(Cnp);
+            return Int32.Parse(Id);
         }
     }
 }
---
+++ ManagingAdmissionContest/Database.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+
+namespace ManagingAdmissionContest
+{
+    /// <summary>
+    /// Manages the more general aspects of database connection for the applicant database.
+    /// </summary>
+    class Database
+    {
+        /// <summary>
+        /// Creates a database.
+        /// </summary>
+        /// <param name="databaseName">The name of the database</param>
+        public static void CreateDatabase(string databaseName)
+        {
+            Directory.CreateDirectory(databaseName);
+        }
+
+        /// <summary>
+        /// Creates a table.
+        /// </summary>
+        /// <param name="tableName">The name of the table</param>
+        public static void CreateTable(string tableName)
+        {
+            try
+            {
+                FileStream fs = File.Create(tableName);
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Deletes a database.
+        /// </summary>
+        /// <param name="databaseName">The name of the database</param>
+        public static void DeleteDatabase(string databaseName)
+        {
+            Directory.Delete(databaseName, true);
+        }
+
+        /// <summary>
+        /// Deletes a table.
+        /// </summary>
+        /// <param name="tableName">The name of the table</param>
+        public static void DeleteTable(string tableName)
+        {
+            try
+            {
+                if (File.Exists(tableName))
+                {
+                    File.Delete(tableName);
+                }
+                else
+                {
+                    throw new FileNotFoundException("File does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
